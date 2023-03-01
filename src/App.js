8e162c6fb1c56b1292fe36fd78b299e9efe0b76a
Vinicius José <viniciusjosedev@gmail.com>
2023-02-28 22:56:41 -0300

    more updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FetchProvider from './context/FetchProvider';
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { useContext, useState } from 'react';
+import { Button, FormGroup, Input, Label } from 'reactstrap';
 import style from '../styles/css/Home.module.css';
 import FetchContext from '../context/FecthContext';
 import Table from './Table';
+import logoStarWars from '../styles/images/logoStarWars.svg';
+import iconSearch from '../styles/images/iconSearch.svg';
 
 function Home() {
   const TAGS_IMUTAVEIS = ['population',
@@ -22,121 +25,143 @@ function Home() {
   const [order, setOrder] = useState({ column: 'population', sort: 'ASC' });
   return (
     <main className={ style.main }>
-      <input
-        type="text"
-        data-testid="name-filter"
-        onChange={ ({ target: { value } }) => funcFilter('inputText', value) }
-      />
-      <select
-        name=""
-        data-testid="column-filter"
-        onChange={ ({ target: { value } }) => setState({ ...state, tag: value }) }
-        id=""
-      >
-        {TAGS_IMUTAVEIS.filter((e) => !tags.includes(e)).map((elemento) => (
-          <option key={ elemento } value={ elemento }>{elemento}</option>
-        ))}
-      </select>
-      <select
-        name=""
-        onChange={ ({ target: { value } }) => setState({ ...state, operator: value }) }
-        data-testid="comparison-filter"
-        id=""
-      >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        type="number"
-        data-testid="value-filter"
-        value={ state.unit }
-        onChange={ ({ target: { value } }) => setState({ ...state, unit: value }) }
-      />
-      <button
-        type="button"
-        onClick={ () => {
-          funcFilter('completFilter', state);
-          setTags([...tags, state.tag]);
-          setState({ ...state,
-            tag: TAGS_IMUTAVEIS
-              .filter((e) => !state.tag.includes(e))[0] });
-          setHistory([...history, state]);
-        } }
-        data-testid="button-filter"
-      >
-        Filtrar
-      </button>
-      <select
-        name=""
-        id=""
-        data-testid="column-sort"
-        onClick={ ({ target: { value } }) => setOrder({ ...order, column: value }) }
-      >
-        {TAGS_IMUTAVEIS.map((elemento) => (
-          <option key={ elemento } value={ elemento }>{elemento}</option>
-        ))}
-      </select>
-      <label htmlFor="ASC">
-        Crescente
-        <input
-          type="radio"
-          name="ordem"
-          onClick={ ({ target: { value } }) => setOrder({ ...order, sort: value }) }
-          value="ASC"
-          data-testid="column-sort-input-asc"
-          id="ASC"
-        />
-      </label>
-      <label htmlFor="DESC">
-        Decrescente
-        <input
-          type="radio"
-          name="ordem"
-          onClick={ ({ target: { value } }) => setOrder({ ...order, sort: value }) }
-          value="DESC"
-          data-testid="column-sort-input-desc"
-          id="DESC"
-        />
-      </label>
-      <button
-        data-testid="column-sort-button"
-        onClick={ () => {
-          funcFilter('ordernar', order);
-          setOrder({ column: 'population', sort: 'ASC' });
-        } }
-        type="button"
-      >
-        Ordenar
-      </button>
-      {tags.map((elemento) => (
-        <div
-          data-testid="filter"
-          key={ elemento }
-        >
-          <button
-            type="button"
-            onClick={ () => {
-              funcFilter(
-                'reverter',
-                history.filter((e) => !e.tag.includes(elemento)),
-              );
-              setTags(tags.filter((e) => e !== elemento));
-              setHistory(history.filter((e) => e.tag !== elemento));
-            } }
-          >
-            {elemento}
-          </button>
+      <img src={ logoStarWars } alt="" />
+      <section>
+        <div className={ style.divFilters }>
+          <div className={ style.divOnlyInputText }>
+            <Input
+              type="text"
+              data-testid="name-filter"
+              onChange={ ({ target: { value } }) => funcFilter('inputText', value) }
+            />
+						<img src={ iconSearch } alt="" />
+          </div>
+          <div className={ style.outerFilters }>
+            <Input
+              type="select"
+              name=""
+							className={ style.inputSelect }
+              data-testid="column-filter"
+              onChange={ ({ target: { value } }) => setState({ ...state, tag: value }) }
+              id=""
+            >
+              {TAGS_IMUTAVEIS.filter((e) => !tags.includes(e)).map((elemento) => (
+                <option key={ elemento } value={ elemento }>{elemento}</option>
+              ))}
+            </Input>
+            <Input
+              type="select"
+              name=""
+							className={ style.inputSelect }
+              onChange={ ({ target: { value } }) => {
+                setState({ ...state, operator: value });
+              } }
+              data-testid="comparison-filter"
+              id=""
+            >
+              <option value="maior que">maior que</option>
+              <option value="menor que">menor que</option>
+              <option value="igual a">igual a</option>
+            </Input>
+            <Input
+              type="number"
+							className={ style.inputNumber }
+              data-testid="value-filter"
+              value={ state.unit }
+              onChange={ ({ target: { value } }) => setState({ ...state, unit: value }) }
+            />
+            <Button
+              type="button"
+							className={ style.button }
+              onClick={ () => {
+                funcFilter('completFilter', state);
+                setTags([...tags, state.tag]);
+                setState({ ...state,
+                  tag: TAGS_IMUTAVEIS
+                    .filter((e) => !state.tag.includes(e))[0] });
+                setHistory([...history, state]);
+              } }
+              data-testid="button-filter"
+            >
+              Filtrar
+            </Button>
+            <Input
+              type="select"
+              name=""
+							className={ style.inputSelect }
+              id=""
+              data-testid="column-sort"
+              onClick={ ({ target: { value } }) => setOrder({ ...order, column: value }) }
+            >
+              {TAGS_IMUTAVEIS.map((elemento) => (
+                <option key={ elemento } value={ elemento }>{elemento}</option>
+              ))}
+            </Input>
+						<div>
+							<FormGroup check>
+								<Label check className={ style.label }>
+									Ascendente
+								</Label>
+								<Input
+									type="radio"
+									className={ style.inputRadio }
+									name="ordem"
+									onClick={ ({ target: { value } }) => setOrder({ ...order, sort: value }) }
+									value="ASC"
+									data-testid="column-sort-input-asc"
+									id="ASC"
+								/>
+							</FormGroup>
+							<FormGroup check>
+								<Label check className={ style.label }>
+									Descendente
+								</Label>
+								<Input
+									type="radio"
+									name="ordem"
+									className={ style.inputRadio }
+									onClick={ ({ target: { value } }) => setOrder({ ...order, sort: value }) }
+									value="DESC"
+									data-testid="column-sort-input-desc"
+									id="DESC"
+								/>
+							</FormGroup>
+						</div>
+            <Button
+              data-testid="column-sort-button"
+							className={ style.button }
+              onClick={ () => {
+                funcFilter('ordernar', order);
+                setOrder({ column: 'population', sort: 'ASC' });
+              } }
+              type="button"
+            >
+              Ordenar
+            </Button>
+					</div>
+            {tags.map((elemento) => (
+              <div
+                data-testid="filter"
+                key={ elemento }
+              >
+                <button
+                  type="button"
+                  onClick={ () => {
+                    funcFilter(
+                      'reverter',
+                      history.filter((e) => !e.tag.includes(elemento)),
+                    );
+                    setTags(tags.filter((e) => e !== elemento));
+                    setHistory(history.filter((e) => e.tag !== elemento));
+                  } }
+                >
+                  {elemento}
+                </button>
+              </div>
+            ))}
         </div>
-      ))}
-      <button
-        data-testid="button-remove-filters"
-        type="submit"
-        onClick={ () => funcFilter('reset') }
-      >
-        Remover Filtros
-      </button>
-      <Table filter={ filter } />
+        <Table filter={ filter } />
+      </section>
     </main>
   );
 }
